c351c458a70c0b4ba5c9f82ea272c5303f5e67c0
Chang1l <4718794@naver.com>
2022-10-23 17:56:27 +0900

    test ver1

---
+++ src/main/java/com/ticket/biz/controller/EmailController.java
@@ -0,0 +1,32 @@
+package com.ticket.biz.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.ticket.biz.service.MailService;
+
+@Controller
+public class EmailController {
+	
+	@Autowired
+	private MailService mailService;
+
+
+	//DB작업이 필요한 만큼 DAO들 선언해야함
+	
+	//아이디와 이메일이 같으면 특정한 메일이 가게..
+	@RequestMapping("/noticeMail")
+	public ModelAndView sendEmail(String id, String email) throws Exception {
+		ModelAndView mv = new ModelAndView();
+		
+		String addr = "chang9840@gmail.com";
+		String subject = "[😀😀] 알림메일 입니다.";
+		String body = "안녕하세요?\r\n소통해요~\r\n SMTP메일 테스트입니다.";
+		mailService.sendEmail(email, addr, subject, body);
+		
+		mv.setViewName("/");
+		return mv;
+	}
+}
---
+++ src/main/webapp/WEB-INF/views/index.jsp
@@ -0,0 +1,14 @@
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<%@ page session="false" %>
+<html>
+<head>
+	<title>Home</title>
+</head>
+<body>
+<h1>
+	Hello world!  
+</h1>
+
+<a href="noticeMail.jsp">rr</a>
+</body>
+</html>
